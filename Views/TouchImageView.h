be3ee49fecfb50ca65f76345041200f4174efcbc
ajie <ajie@github.com>
2018-02-07 15:58:56 +0800

    可以随意缩放旋转移动的image view
    自定义UIImageview

---
+++ Views/TouchImageView.h
@@ -0,0 +1,13 @@
+//
+//  TouchImageView.h
+//  BeautyVideo
+//
+//  Created by 阿杰 on 2018/2/7.
+//  Copyright © 2018年 baiwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchImageView : UIImageView<UIGestureRecognizerDelegate>
+
+@end
---
+++ Views/TouchImageView.m
@@ -0,0 +1,149 @@
+//
+//  TouchImageView.m
+//  BeautyVideo
+//
+//  Created by 阿杰 on 2018/2/7.
+//  Copyright © 2018年 baiwang. All rights reserved.
+//
+
+#import "TouchImageView.h"
+
+@implementation TouchImageView{
+    CGFloat x,y,s,r;
+
+}
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self makeTouchEventEnable];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self makeTouchEventEnable];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self makeTouchEventEnable];
+    }
+    return self;
+}
+
+
+-(void)makeTouchEventEnable{
+    x=y=r=0;
+    s=1;
+    [self setUserInteractionEnabled:YES];
+    
+    [self setMultipleTouchEnabled:YES];
+    UIPanGestureRecognizer *gestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGestureRecognizer:)];
+    gestureRecognizer.delegate = self;
+    gestureRecognizer.maximumNumberOfTouches =1;
+    [self addGestureRecognizer:gestureRecognizer];
+    
+    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchGestureRecognizer:)];
+    pinchGestureRecognizer.delegate = self;
+    [self addGestureRecognizer:pinchGestureRecognizer];
+    UIRotationGestureRecognizer *rotationGestureRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotationGestureRecognizer:)];
+    rotationGestureRecognizer.delegate = self;
+    [self addGestureRecognizer:rotationGestureRecognizer];
+    UILongPressGestureRecognizer *longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGestureRecognizer:)];
+    longPressGestureRecognizer.delegate = self;
+    [self addGestureRecognizer:longPressGestureRecognizer];
+}
+
+
+-(void)handlePanGestureRecognizer:(UIPanGestureRecognizer*)pan{
+    switch (pan.state) {
+        case UIGestureRecognizerStateBegan:
+            x = y =0;
+            break;
+        default:
+            break;
+    }
+    CGPoint point = [pan translationInView:pan.view];
+    [self printTransform:self.transform];
+    NSLog(@"dx=%f----dy=%f",(point.x) - x,(point.y) - y);
+
+    self.transform = CGAffineTransformTranslate(self.transform, (point.x) - x,(point.y) - y);
+    x = point.x;
+    y = point.y;
+    NSLog(@"x=%f----y=%f",x,y);
+    [self printTransform:self.transform];
+}
+
+
+-(void)printTransform:(CGAffineTransform)tansform{
+    
+    NSLog(@"transform : a = %f----b = %f ---- c = %f ---- d = %f ---- tx = %f ---- ty = %f",tansform.a,tansform.b,tansform.c,tansform.d,tansform.tx,tansform.ty);
+}
+
+-(void)handlePinchGestureRecognizer:(UIPinchGestureRecognizer*)pinch{
+    switch (pinch.state) {
+        case UIGestureRecognizerStateBegan:
+            s = 1;
+            break;
+        default:
+            break;
+    }
+    self.transform = CGAffineTransformScale(self.transform, pinch.scale/s, pinch.scale/s);
+    
+    s = pinch.scale;
+    
+    NSLog(@"s=%f]",s);
+    //            [self makeTransformWithType:2];
+    [self printTransform:self.transform];
+    NSLog(@"======================================");
+
+}
+-(void)handleRotationGestureRecognizer:(UIRotationGestureRecognizer*)rotation{
+    switch (rotation.state) {
+        case UIGestureRecognizerStateBegan:
+            r = 0;
+            break;
+        default:
+            break;
+    }
+    NSLog(@"======================================");
+    self.transform = CGAffineTransformRotate(self.transform, rotation.rotation  - r);
+    r = rotation.rotation;
+    NSLog(@"r=%f]",r);
+    //             [self makeTransformWithType:3];
+    [self printTransform:self.transform];
+    NSLog(@"======================================");
+
+}
+-(void)handleLongPressGestureRecognizer:(UILongPressGestureRecognizer*)longPress{
+    
+}
+
+
+
+
+
+
+
+
+//手指触摸屏幕后回调的方法，返回NO则不再进行手势识别，方法触发等
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+    return YES;
+}
+//开始进行手势识别时调用的方法，返回NO则结束，不再触发手势
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+    return YES;
+}
+//是否支持多时候触发，返回YES，则可以多个手势一起触发方法，返回NO则为互斥
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+    return YES;
+}
+@end
