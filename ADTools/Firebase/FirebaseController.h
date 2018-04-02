f929dd609a5d12a7b775e868cc67e91cc3510aa0
ajie <ajie@github.com>
2018-04-02 10:55:01 +0800

    Firebase方法更新
    增加对广告的多字段控制：
    同时按比率，按间隔次数控制；
    同时按比率，按间隔次数，按每天出现总数控制

--- ADTools/Firebase/FirebaseController.h
+++ ADTools/Firebase/FirebaseController.h
@@ -21,6 +21,13 @@
  nanopb.framework
  Protobuf.framework
  
+ cocopod导入
+ platform :ios, '8.0'
+ target '******' do
+ pod 'Firebase/RemoteConfig'
+ pod 'Firebase/Core'
+ end
+ 
  添加两个plist文件
  GoogleService-Info.plist//从后台下载
  RemoteConfigDefaults.plist//可以从别的软件导入
@@ -32,36 +39,87 @@
 //3、根据需要实现loadAD代理方法。具体控制方法可参考一下例子
 //
 /*******************例子：*********
+ //加载banner广告
  -(void)loadAD{
- if([[FirebaseController manager] canLoadADByAdRatio:ad_ratio_album])
+    if([[FirebaseController manager] canLoadADByAdRatio:ad_ratio_album])
+     {
+            //具体加载广告的代码
+             _adview =[self.adManaer createAlbumAdViewWithViewController:self withFrame:CGRectMake(0, kSHeight - 50 - self.backgroundView.height, kSWidth, 50)];
+             [self.view addSubview:_adview];
+     }
+ }
+ //加载插屏广告
+ -(void)loadAD{
+ if([[FirebaseController manager] canLoadADByAdRatio:ad_ratio_album andTimeRaio:ad_ratio_album_times])
  {
  //具体加载广告的代码
- _adview =[self.adManaer createAlbumAdViewWithViewController:self withFrame:CGRectMake(0, kSHeight - 50 - self.backgroundView.height, kSWidth, 50)];
- [self.view addSubview:_adview];
+ [self.adManaer createInterstitialAdWithDeletage:self withID:ad_id];
  }
+ }
+ //广告加载完成需要做一些操作
+ -(void)interstitialDidReceiveAd:(GADInterstitial *)ad{
+ if (ad.isReady) {
+ if ([ad.adUnitID isEqualToString:ad_id])
+ resAD = YES;
+ }
+ }
+ //如果需要多次展示，请重复加载
+ -(void)interstitialDidDismissScreen:(GADInterstitial *)ad{
+ if ([ad.adUnitID isEqualToString:ad_id]) {
+ [self loadResAD];
+ }
+ }
+ //在需要展示的位置展示广告
+ -(BOOL)showResAD{
+ if (resAD) {
+ if([[FirebaseController manager] canLoadADByAdRatio:nil  andTimeRaio:ad_ratio_album_times andDayTimeRatio:ad_ratio_album_days andDefaultDayTimeRatio:15])
+ {
+ //具体加载广告的代码
+ [[ADManager manager] showInterstitialAdWithViewController:self withID:ad_id];
+ resAD = NO;
+ return YES;
+ }
+ }
+ return NO;
  }
  */
 //4.在viewdidload方法中调用loadAD;
+//5、如果需要每隔一段时间自动加载广告，则实现FirebaseControllerDeletage
+//6、如果需要加入去广告控制，则实现RemoveADDelegate
 
 
 #import <Foundation/Foundation.h>
 #import "Firebase.h"
-#define instabox_main_ad_ratio @"instabox_main_ad_ratio"
-#define instabox_album_ad_ratio @"instabox_album_ad_ratio"
-#define instabox_share_ad_ratio @"instabox_share_ad_ratio"
-#define instabox_fb_ad_ratio @"instabox_fb_ad_ratio"
-
+@protocol RemoveADDelegate;
 
 @protocol FirebaseControllerDeletage;
 @interface FirebaseController : NSObject
 @property(nonatomic,weak) id<FirebaseControllerDeletage> deletage;
+@property (nonatomic,weak) id<RemoveADDelegate> removeAdDelegate;
 @property(nonatomic,strong) NSMutableArray * array;
 @property (nonatomic,strong) FIRRemoteConfig *remoteConfig;
 +(FirebaseController*)manager;
+//ad字段控制广告比率
 -(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio;
+//time字段控制每天最多显示次数 0代表无，100代表无限制，1-99控制次数
+-(NSInteger)numberOfOneDayCanLoadByTimeRatio:(NSString *) time_ratio;
+//ad与time字段同时控制广告能否显示，每天每三次弹一次
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *) time_ratio;
+//ad与time字段同时控制广告能否显示，每天每time次弹一次
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *) time_ratio andTime:(NSInteger) time;
+//ad与time,dayTime_ratio字段同时控制广告能否显示
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *)time_ratio andDayTimeRatio:(NSString*)dayTime_ratio;
+
+//ad与time,dayTime_ratio字段同时控制广告能否显示
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *)time_ratio andDayTimeRatio:(NSString *)dayTime_ratio andDefaultDayTimeRatio:(NSInteger)defaultDayTimeRatio;
 @end
 @protocol FirebaseControllerDeletage<NSObject>
-@optional
+@required
 -(void)loadAD;
+@optional
+-(void)onFirebaseReceivedDate;
 @end
-
+@protocol RemoveADDelegate<NSObject>
+@required
+-(BOOL)canLoadAD;
+@end
--- ADTools/Firebase/FirebaseController.m
+++ ADTools/Firebase/FirebaseController.m
@@ -27,34 +27,32 @@ static FirebaseController* manager;
         return;
     }
     self.remoteConfig = [FIRRemoteConfig remoteConfig];
-    
+
     FIRRemoteConfigSettings *remoteConfigSettings = [[FIRRemoteConfigSettings alloc] initWithDeveloperModeEnabled:NO];
     self.remoteConfig.configSettings = remoteConfigSettings;
-    
+
     [self.remoteConfig setDefaultsFromPlistFileName:@"RemoteConfigDefaults"];
-    
+
     [self fetchConfig];
 }
 -(NSMutableArray *)array{
     if (!_array) {
         _array = [NSMutableArray array];
-
+        
+        
         //在下面添加配置的ad_ratio
-        [_array addObject:instabox_fb_ad_ratio];
-        [_array addObject:instabox_main_ad_ratio];
-        [_array addObject:instabox_album_ad_ratio];
-        [_array addObject:instabox_share_ad_ratio];
+
     }
     return _array;
 }
 - (void)fetchConfig {
-    
+
     long expirationDuration = 3600;
-    
+
     if (self.remoteConfig.configSettings.isDeveloperModeEnabled) {
         expirationDuration = 0;
     }
-    
+
     [self.remoteConfig fetchWithExpirationDuration:expirationDuration completionHandler:^(FIRRemoteConfigFetchStatus status, NSError *error) {
         if (error) {
             NSLog(@"%@",error);
@@ -77,8 +75,11 @@ static FirebaseController* manager;
         }
         [def synchronize];
         [self loadAD];
+        if (self.deletage && [self.deletage respondsToSelector:@selector(onFirebaseReceivedDate)]) {
+            [self.deletage onFirebaseReceivedDate];
+        }
     }];
-    
+ 
 }
 
 - (BOOL)isPureInt:(NSString *)string{
@@ -88,8 +89,18 @@ static FirebaseController* manager;
 }
 
 -(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio{
+    BOOL canloadAD = YES;
+    if (self.removeAdDelegate && [self.removeAdDelegate respondsToSelector:@selector(canLoadAD)]) {
+        canloadAD = [self.removeAdDelegate canLoadAD];
+    }
+    if (canloadAD == NO) {
+        return NO;
+    }
     NSUserDefaults * def = [NSUserDefaults standardUserDefaults];
-    NSString * adRatioStr = [def objectForKey:ad_ratio];//根据具体的需求获取控制标志。
+    NSString * adRatioStr = @"100";
+    if (ad_ratio) {
+        adRatioStr = [def objectForKey:ad_ratio];//根据具体的需求获取控制标志。
+    }
     if (!adRatioStr) {
         adRatioStr = @"100";
     }
@@ -108,11 +119,117 @@ static FirebaseController* manager;
     }
     return NO;
 }
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *) time_ratio{
+    return [self canLoadADByAdRatio:ad_ratio andTimeRaio:time_ratio andTime:3];
+}
+
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *)time_ratio andTime:(NSInteger)dayTime{
+    if ([self canLoadADByAdRatio:ad_ratio]) {
+        NSString * numberKey = [time_ratio stringByAppendingString:@"_number"];
+        NSString * dateKey = [time_ratio stringByAppendingString:@"_date"];
+        NSString * timeKey = [time_ratio stringByAppendingString:@"_time"];
+        if (!time_ratio) {
+            numberKey = [time_ratio stringByAppendingString:@"default_number"];
+            dateKey = [time_ratio stringByAppendingString:@"default_date"];
+            timeKey = [time_ratio stringByAppendingString:@"default_time"];
+        }
+        NSUserDefaults * def = [NSUserDefaults standardUserDefaults];
+        NSInteger number = [def integerForKey:numberKey];//当天出现的次数。
+        NSInteger time = [def integerForKey:timeKey];//当天出现的次数。
+        NSDate * date = [def objectForKey:dateKey];//上次出现的日期。
+        if (!date) {
+            number = 0;
+            date = [NSDate date];
+        }
+        if([self numberOfDaysAtDate:date andDate:[NSDate date]] >0){
+            number = 0;
+            date = [NSDate date];
+            time = 0;
+        }
+        if (number < [self numberOfOneDayCanLoadByTimeRatio:time_ratio]) {
+            if (time % dayTime == dayTime -1) {
+                number ++;
+                [def setInteger:number forKey:numberKey];
+                [def setObject:date forKey:dateKey];
+                time = 0;
+                [def setInteger:time forKey:timeKey];
+                return YES;
+            }
+            time ++;
+            [def setInteger:time forKey:timeKey];
+        }
+        NSLog(@"number = %ld   daytime = %ld   time = %ld  max = %ld",number,dayTime,time,[self numberOfOneDayCanLoadByTimeRatio:time_ratio]);
+    }
+    return NO;
+}
+
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *)time_ratio andDayTimeRatio:(NSString *)dayTime_ratio{
+    return [self canLoadADByAdRatio:ad_ratio andTimeRaio:time_ratio andDayTimeRatio:dayTime_ratio andDefaultDayTimeRatio:6];
+}
+
+-(BOOL)canLoadADByAdRatio:(NSString *)ad_ratio andTimeRaio:(NSString *)time_ratio andDayTimeRatio:(NSString *)dayTime_ratio andDefaultDayTimeRatio:(NSInteger)defaultDayTimeRatio{
+    NSUserDefaults * def = [NSUserDefaults standardUserDefaults];
+    NSString * adRatioStr = [NSString stringWithFormat:@"%ld",defaultDayTimeRatio];
+    if (dayTime_ratio) {
+        adRatioStr = [def objectForKey:dayTime_ratio];//根据具体的需求获取控制标志。
+    }
+    if (!adRatioStr) {
+        adRatioStr = [NSString stringWithFormat:@"%ld",defaultDayTimeRatio];
+    }
+    int adRatio = [adRatioStr intValue];
+    return [self canLoadADByAdRatio:ad_ratio andTimeRaio:time_ratio andTime:adRatio];
+}
+
+-(NSInteger)numberOfDaysAtDate:(NSDate *) firstData andDate:(NSDate*)secondData{
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [gregorian setFirstWeekday:2];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate *fromDate;
+    NSDate *toDate;
+    [gregorian rangeOfUnit:NSCalendarUnitDay startDate:&fromDate interval:NULL forDate:firstData];
+    [gregorian rangeOfUnit:NSCalendarUnitDay startDate:&toDate interval:NULL forDate:secondData];
+    NSDateComponents *dayComponents = [gregorian components:NSCalendarUnitDay fromDate:fromDate toDate:toDate options:0];
+    return labs(dayComponents.day);
+}
+
+-(NSInteger)numberOfOneDayCanLoadByTimeRatio:(NSString *) time_ratio{
+    NSInteger number = NSIntegerMax;
+    if (self.removeAdDelegate && [self.removeAdDelegate respondsToSelector:@selector(canLoadAD)]) {
+        number = [self.removeAdDelegate canLoadAD]?number:0;
+    }
+    NSUserDefaults * def = [NSUserDefaults standardUserDefaults];
+    NSString * adRatioStr = @"3";
+    if (time_ratio) {
+        adRatioStr = [def objectForKey:time_ratio];//根据具体的需求获取控制标志。
+    }
+    if (!adRatioStr) {
+        adRatioStr = @"3";
+    }
+    int adRatio = [adRatioStr intValue];
+    if(adRatio == 100)//100 显示广告
+    {
+        number = NSIntegerMax;
+    }else{
+        number = adRatio;
+    }
+    return number;
+}
 
 -(void)loadAD{
+    if (self.removeAdDelegate && [self.removeAdDelegate respondsToSelector:@selector(canLoadAD)]) {
+        if (![self.removeAdDelegate canLoadAD]) {
+            return;
+        }
+    }
     if (self.deletage && [self.deletage respondsToSelector:@selector(loadAD)]) {
         [self.deletage loadAD];
     }
+
+}
+
+-(void)dealloc{
+    [[NSUserDefaults standardUserDefaults]synchronize];
 }
 @end
-
